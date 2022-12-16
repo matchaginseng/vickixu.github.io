40b59cd3867ee85c79751652e3543f05d723541d
VICKI XU <VICKIXU@COLLEGE.HARVARD.EDU>
2022-12-16 15:49:00 -0800

    migrate to ts lmao

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,12 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
-export default {
+module.exports = {
   mount: {
-     public: '/',
-     src: '/dist',
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
   },
-  plugins: [
-    '@snowpack/plugin-react-refresh'
-  ],
+  plugins: ['@snowpack/plugin-typescript'],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
